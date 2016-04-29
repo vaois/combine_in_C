b12349e7fde211770280d19f0283647582bbeb53
vaois_ping <851149343@qq.com>
2016-04-29 11:25:32 +0800

    add combine.c

---
+++ combine.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main(int argc,char*argv[])
+{
+	char c;
+	int in1,in2,out;
+    int length;
+
+	in1 = open(argv[1],O_RDONLY);
+	in2 = open(argv[2],O_RDONLY);
+	out = open(argv[3], O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
+
+	length = lseek(in1,0,SEEK_END);
+	lseek(in1,0,SEEK_SET);
+	while(read(in1,&c,1) == 1)
+		write(out,&c,1);
+	close(in1);
+	lseek(out,length+1,SEEK_SET);
+	while(read(in2,&c,1) == 1)
+		write(out,&c,1);
+	close(in2);
+	close(out);
+	exit(0);
+
+
+
+
+
+}
